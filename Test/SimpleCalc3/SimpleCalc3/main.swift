99a46fdd5abd53f7819e214433aa599e2b0b13b3
Nathan Han <nh0713@uw.edu>
2018-10-18 16:32:03 -0700

    solution

---
+++ Test/SimpleCalc3/SimpleCalc3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc3
+//
+//  Created by Nathan Han on 10/3/18.
+//  Copyright © 2018 Nathan Han. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
