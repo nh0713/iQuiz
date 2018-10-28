920a65f98b0d6b3fba59622d92c80b4b6f105090
Nathan Han <nh0713@uw.edu>
2018-10-28 14:41:23 -0700

    first push

---
+++ iQuiz/iQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Nathan Han on 10/28/18.
+//  Copyright © 2018 Nathan Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
